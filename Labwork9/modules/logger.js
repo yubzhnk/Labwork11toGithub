9705ed7bb7adc2d602f670014562a2a8adb6de71
yubzhnk <21fi.yu.bozhenko@std.npu.edu.ua>
2023-05-08 22:47:58 +0300

    Update task

--- Labwork9/modules/logger.js
+++ Labwork9/modules/logger.js
@@ -1,4 +1,34 @@
+//1. Напишіть простий модуль логування, який підтримує логування повідомлень, для яких
+//рівень логування перевищує заданий поріг. Експортуйте функцію log, константи рівнів
+//логування та функцію завдання рівня логування.
 
+const LogLevel = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  verbose: 3,
+  debug: 4,
+  silly: 5,
+};
+
+let currLogLevel = LogLevel.debug;
+
+function setLogLevel(logLevel) {
+  currLogLevel = logLevel;
+}
+
+function log(level, message) {
+  if (level < currLogLevel) return;
+
+  const timestamp = new Date().toISOString();
+  console.log(`${timestamp} [${level}] ${message}`);
+}
+
+module.exports = {
+  LogLevel,
+  setLogLevel,
+  log,
+}; 
 
 //2. Повторіть попереднє завдання але тепер експортуйте весь класс по замовчуванню.
   
